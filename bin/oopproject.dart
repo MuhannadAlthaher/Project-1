8c5e8579b67a5f0d75e092df1b4e41cdcc3431c6
muhannad <mohnsalzaher@gmail.com>
2024-08-02 23:22:06 +0300

    create project

---
+++ bin/oopproject.dart
@@ -0,0 +1,5 @@
+import 'package:oopproject/oopproject.dart' as oopproject;
+
+void main(List<String> arguments) {
+  print('Hello world: ${oopproject.calculate()}!');
+}
---
+++ test/oopproject_test.dart
@@ -0,0 +1,8 @@
+import 'package:oopproject/oopproject.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
