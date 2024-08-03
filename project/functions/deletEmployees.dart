cfd76ee348e380378738a927137e8d9a0c86eb0a
muhannad <mohnsalzaher@gmail.com>
2024-08-03 16:31:37 +0300

    add function(deletEmployees in functions folder)

---
+++ project/functions/deletEmployees.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+import '../modules/employees.dart';
+
+deletEmployees(List<Employee> employyes) {
+  print("Kindly enter the ID number: "); //asked the for th ID number
+  var checkID = stdin.readLineSync()!; //get the input from user
+
+  employyes.removeWhere((employyes) =>
+      employyes.id ==
+      checkID); //here I used remove where function that check if the ID from user equal the ID in the list then it will remove it
+  print("employee deleted succffuly");
+  print("____________________________________");
+}
