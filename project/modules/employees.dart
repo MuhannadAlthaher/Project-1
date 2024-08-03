75bc6dc7cbad547e0d16f092326eceb8da7ec1fc
muhannad <mohnsalzaher@gmail.com>
2024-08-03 16:24:00 +0300

    Class Employee added with varibles and constractor and mithod to view the objects

---
+++ project/modules/employees.dart
@@ -0,0 +1,23 @@
+class Employee {
+  final String? id;
+  final String? name;
+  String? salary;
+  String? postion;
+  String? jobDescription;
+  List permissions;
+//Class to store empolyess data with constarcter
+  Employee({
+    required this.id,
+    required this.name,
+    required this.salary,
+    required this.postion,
+    required this.jobDescription,
+    required this.permissions,
+  });
+//this function will view all the data for employes
+  void viewEmp() {
+    print(
+        " Employee Id :$id \n Employee name: $name \n employee Salary:$salary \n employee Postion: $postion \n Job decription:$jobDescription \n ermissions: ${permissions.join(" ")}");
+    print("----------------------------------");
+  }
+}
